295b704193105903eac8d9218adc888dc1bb62ad
xaterz <ruofan.yu@gmail.com>
2016-04-06 19:33:58 +0800

    Made some adjustments

--- SelectionMutation.java
+++ SelectionMutation.java
@@ -6,8 +6,9 @@ public class SelectionMutation {
 	private ArrayList<double[]> weightSetsList;
 	private ArrayList<Double> fitnessScoreList;
 	private ArrayList<double[]> childWeightSets;
+	private ArrayList<Double> childFitnessScores;
 	private ArrayList<double[]> newPopulation;
-	private Random rand;
+	private Random rand = new Random();
 
 	public SelectionMutation(ArrayList<double[]> weightSetsList, ArrayList<Double> fitnessScoreList) {
 		this.weightSetsList = weightSetsList;
@@ -17,6 +18,7 @@ public class SelectionMutation {
 
 	private void produceOffspring() {
 		childWeightSets = new ArrayList<double[]>();
+		childFitnessScores = new ArrayList<Double>();
 		tournamentSelection();
 	}
 
@@ -28,6 +30,7 @@ public class SelectionMutation {
 
 		int i;
 		for (i = 0; i < numberOfOffspring; i++) {
+			System.out.print((i+1) + ") ");
 			ArrayList<double[]> twoParents = findingBestTwo(population, sizeOfEachBracket);
 			double[] offspring = newOffspring(twoParents);
 			childWeightSets.add(offspring);
@@ -49,9 +52,11 @@ public class SelectionMutation {
 		bestTwoWeightSets.add(0, new double[6]);
 		bestTwoWeightSets.add(1, new double[6]);
 		int i;
+		System.out.print("Bracket: ");
 		for (i = 0; i < sizeOfEachBracket; i++) {
 			int position = rand.nextInt(population);
 			double fitnessScore = fitnessScoreList.get(position);
+			System.out.print(fitnessScore + " ");
 			if (fitnessScore > bestTwoScores[0]) {
 				bestTwoScores[1] = bestTwoScores[0];
 				bestTwoScores[0] = fitnessScore;
@@ -62,6 +67,17 @@ public class SelectionMutation {
 				bestTwoWeightSets.set(1, weightSetsList.get(position));
 			}
 		}
+		System.out.println("\nBest 2 Scores: " + bestTwoScores[0] + ", " + bestTwoScores[1]);
+		System.out.print("Parent Weights: [");
+		for (int j = 0; j < 5; j++) {
+			System.out.print(bestTwoWeightSets.get(0)[j] + ", ");
+		}
+		System.out.print(bestTwoWeightSets.get(0)[5] + "], [");
+		for (int j = 0; j < 5; j++) {
+			System.out.print(bestTwoWeightSets.get(1)[j] + ", ");
+		}
+		System.out.print(bestTwoWeightSets.get(1)[5] + "]");
+		
 		ArrayList<double[]> parentsWithFitnessScores = new ArrayList<double[]>();
 		parentsWithFitnessScores.addAll(bestTwoWeightSets);
 		parentsWithFitnessScores.add(2, bestTwoScores);
@@ -81,7 +97,12 @@ public class SelectionMutation {
 			offspringWeightSet[i] = (parentAScore/ (parentAScore+parentBScore)) * parentAWeight[i]
 					+ (parentBScore/ (parentAScore+parentBScore)) * parentBWeight[i];
 		}
-
+		System.out.print("\nChild Weight: ");
+		for (i = 0; i < offspringWeightSet.length-1; i++) {
+			System.out.print(offspringWeightSet[i] + ", ");
+		}
+		System.out.println(offspringWeightSet[5] + "]\n");
+		
 		// 5% chance for mutation to occur
 		if (rand.nextInt(20) == 0) {
 			offspringWeightSet = mutation(offspringWeightSet);
@@ -91,17 +112,26 @@ public class SelectionMutation {
 	}
 
 	private double[] mutation(double[] offspringWeight) {
+		System.out.print("MUTATION! [");
 		double[] mutatedOffspring = offspringWeight;
 		int selectAttribute = rand.nextInt(6);
 		int sign = (int) Math.pow(-1, rand.nextInt(2) + 1);
 		// scale of mutation is limited within the range of +- 0.2
 		double mutatedAttribute = offspringWeight[selectAttribute] + sign * 0.2 * Math.random();
 		mutatedOffspring[selectAttribute] = mutatedAttribute;
+		for (int i = 0; i < offspringWeight.length-1; i++) {
+			System.out.print(offspringWeight[i] + ", ");
+		}
+		System.out.println(offspringWeight[5] + "]");
 		return mutatedOffspring;
 	}
 	
 	public ArrayList<double[]> getChildWeightSets() {
 		return this.childWeightSets;
+	}
+	
+	public ArrayList<Double> getChildFitnessScores() {
+		return this.childFitnessScores;
 	}
 	
 	public ArrayList<double[]> getNewPopulation() {
